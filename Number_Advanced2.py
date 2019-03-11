a60e8acb8a3209de61c719c7adce810acb710656
Perturabo <36324325+VulkanLives@users.noreply.github.com>
2019-03-11 23:16:27 +0000

    Update Number_Advanced2.py
    Using functions and "If else" statement

--- Number_Advanced2.py
+++ Number_Advanced2.py
@@ -1,57 +1,55 @@
-def addition (number1 ,number2):
+def addition(number1, number2):  # function to add to the numbers it is passed
     num1 = number1
     num2 = number2
     result = num1 + num2
-    print(result)
+    print("Your answer is: ", result)
 
-def Subtraction (number1 ,number2):
+
+def Subtraction(number1, number2):  # function to subtract to the numbers it is passed
     num1 = number1
     num2 = number2
     result = num1 - num2
-    print(result)
+    print("Your answer is: ", result)
 
-def Divide (number1 ,number2):
+
+def Divide(number1, number2):  # function to divide to the numbers it is passed
     num1 = number1
     num2 = number2
     result = num1 / num2
-    print(result)
+    print("Your answer is: ", result)
 
-def Multiply (number1 ,number2):
+
+def Multiply(number1, number2):  # function to multiply to the numbers it is passed
     num1 = number1
     num2 = number2
     result = num1 * num2
-    print(result)
+    print("Your answer is: ", result)
+
 
 userInput1 = int(input("Enter your first number: "))
 userInput2 = int(input("Enter your second number: "))
 
 print("what would like to do with these numbers?")
-userChoice =int(input("<1> = addition, <2> = subtraction, <3> = divsion, <4> = multiply"))
-print(userChoice,"<----TEST CASE, FOR DEBUG PURPOSES ONLY")
+userChoice = int(input("<1> = addition, <2> = subtraction, <3> = divsion, <4> = multiply"))
+print(userChoice, "<----TEST CASE, FOR DEBUG PURPOSES ONLY")
 
 if userChoice == 1:
     print("you chose to add")
-    addition(userInput1,userInput2)
+    addition(userInput1, userInput2)
 else:
     if userChoice == 2:
         print("you chose to subract")
-        Subtraction(userInput1,userInput2)
+        Subtraction(userInput1, userInput2)
     else:
         if userChoice == 3:
             print("you chose to divide")
-            Divide(userInput1,userInput2)
+            Divide(userInput1, userInput2)
         else:
-            if userChoice ==4:
+            if userChoice == 4:
                 print("you chose to multilpy")
-                Multiply(userInput1,userInput2)
+                Multiply(userInput1, userInput2)
             else:
-                print("guess you don't play by the rules") #IF THE USER DOESN'T A PRE-DESCRIBED OPTION, EVERYTHING ELSE SHOULD SPEAK FOR ITSELF
-
-
-
-
-
-
-
+                print(
+                    "guess you don't play by the rules")  # IF THE USER DOESN'T A PRE-DESCRIBED OPTION, EVERYTHING ELSE SHOULD SPEAK FOR ITSELF
 
 
