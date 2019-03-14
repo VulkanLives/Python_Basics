a199a69cf8144e30e6ef199a9516c130fdacdacb
Perturabo <36324325+VulkanLives@users.noreply.github.com>
2019-03-14 23:42:15 +0000

    Update NumberCruncher.py

--- NumberCruncher.py
+++ NumberCruncher.py
@@ -28,6 +28,17 @@ def notZero (number):  #checks if number is 0
         zero_check = True
     else:
          zero_check = False
-
     return zero_check
+
+def confirmInt(userResponse): #CHECKS THAT WHATEVER ITS PASSED IS A INT/NUMBER, NEEDS TWEAKING TO CHECK FOR OTHER DATA/VAR TYPES
+    response = userResponse #takes data it has been passed and stores it in variable "response"
+    check = bool = False      #creates a true or false variable
+    try:
+        val = int(response) #simply checks that response is an INT
+        check = True
+    except ValueError:   # ValueError is pre-defined, hence the caps on V and E , basically says anything "Try" can't do then its an error
+       check = False
+       print("That's not an int!")
+       print("No.. input string is not an Integer. It's a string")
+    return check
 
